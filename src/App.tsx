c9fb3096690cad01467fc9d78f776d0a5c23a293
Demiverse <92321739+Demiverse@users.noreply.github.com>
2023-06-17 13:31:43 +0300

    New

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Routes,
+  Route,
+  useNavigationType,
+  useLocation,
+} from "react-router-dom";
+import Component from "./pages/Component";
+import FrameComponent from "./pages/FrameComponent";
+import FrameComponent1 from "./pages/FrameComponent1";
+import FrameComponent2 from "./pages/FrameComponent2";
+import FrameComponent3 from "./pages/FrameComponent3";
+import FrameComponent4 from "./pages/FrameComponent4";
+import FrameComponent5 from "./pages/FrameComponent5";
+import { useEffect } from "react";
 
 function App() {
+  const action = useNavigationType();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action, pathname]);
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    switch (pathname) {
+      case "/":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/frame-283":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/frame-282":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/frame-281":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/frame-280":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/frame-284":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/frame-285":
+        title = "";
+        metaDescription = "";
+        break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag: HTMLMetaElement | null = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Component />} />
+      <Route path="/frame-283" element={<FrameComponent />} />
+      <Route path="/frame-282" element={<FrameComponent1 />} />
+      <Route path="/frame-281" element={<FrameComponent2 />} />
+      <Route path="/frame-280" element={<FrameComponent3 />} />
+      <Route path="/frame-284" element={<FrameComponent4 />} />
+      <Route path="/frame-285" element={<FrameComponent5 />} />
+    </Routes>
   );
 }
-
 export default App;
