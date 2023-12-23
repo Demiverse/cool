92cf5e3406d4e78d804096f5a37a9d709d8f11b8
Alecksandr Pestretzov <Alecksandr1999@yandex.ru>
2023-12-23 22:36:14 +0300

    created useSound hook to handle background sound

---
+++ src/hooks/useSound.ts
@@ -0,0 +1,37 @@
+import { useState, useEffect, useRef } from "react";
+
+const useSound = (src: string) => {
+  const [muted, setMuted] = useState<boolean>(false);
+  const audioRef = useRef<HTMLAudioElement>(new Audio(src));
+  audioRef.current.volume = 0.12;
+  audioRef.current.loop = true;
+  audioRef.current.muted = muted;
+
+  useEffect(() => {
+		if (audioRef.current) audioRef.current.play();
+		return () => {
+      if (audioRef.current) audioRef.current.pause();
+    }
+	}, []);
+
+  const toggleMuteBgSound = () => {
+    setMuted(!muted);
+    audioRef.current.muted = !muted;
+  };
+  
+  const pauseBgSound = () => {
+    audioRef.current.pause();
+  };
+
+  const returnedValue: any = [
+    muted,
+    {
+      pauseBgSound,
+      toggleMuteBgSound,
+    }
+  ];
+
+  return returnedValue;
+}
+
+export default useSound;
