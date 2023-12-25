15f92b9ee65c5d76acf2958cf4c1db2f25938a48
Alecksandr Pestretzov <Alecksandr1999@yandex.ru>
2023-12-25 05:17:53 +0300

    properly named pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
 import { Routes, Route, useNavigationType, useLocation } from "react-router-dom";
-import Main from "./pages/main/Main";
+import Main from "./pages/main";
 import { default as Yasker } from "./pages/yasker";
 import { default as Ann } from "./pages/ann";
 import { default as Nayan } from "./pages/nayan";
 import { default as Smeyana } from "./pages/smeyana";
-import CharactersPage from "./pages/characters";
+import Characters from "./pages/characters";
 import { WithScroll } from "./components/scrollcontext";
 
 function App() {
@@ -28,19 +28,19 @@ function App() {
 				title = "";
 				metaDescription = "";
 				break;
-			case "/frame-283":
+			case "/yasker":
 				title = "";
 				metaDescription = "";
 				break;
-			case "/frame-282":
+			case "/ann":
 				title = "";
 				metaDescription = "";
 				break;
-			case "/frame-281":
+			case "/nayan":
 				title = "";
 				metaDescription = "";
 				break;
-			case "/frame-280":
+			case "/smeyana":
 				title = "";
 				metaDescription = "";
 				break;
@@ -48,7 +48,7 @@ function App() {
 				title = "";
 				metaDescription = "";
 				break;
-			case "/frame-285":
+			case "/characters":
 				title = "";
 				metaDescription = "";
 				break;
@@ -72,11 +72,11 @@ function App() {
 		<WithScroll>
 			<Routes>
 				<Route path="/" element={<Main />} />
-				<Route path="/frame-283" element={<Yasker />} />
-				<Route path="/frame-282" element={<Ann />} />
-				<Route path="/frame-281" element={<Nayan />} />
-				<Route path="/frame-280" element={<Smeyana />} />
-				<Route path="/frame-285" element={<CharactersPage />} />
+				<Route path="/yasker" element={<Yasker />} />
+				<Route path="/ann" element={<Ann />} />
+				<Route path="/nayan" element={<Nayan />} />
+				<Route path="/smeyana" element={<Smeyana />} />
+				<Route path="/characters" element={<Characters />} />
 			</Routes>
 		</WithScroll>
 	);
