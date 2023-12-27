9aa0d09602f73fb814b3d1d161852cdaa765cb9a
termorey <is_front@mail.ru>
2023-12-27 22:24:44 +0300

    feature: changed audio usage

--- src/hooks/useSound.ts
+++ src/hooks/useSound.ts
@@ -1,26 +1,57 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
+import { useAudio } from "../shared/providers/audio";
 
 const useSound = (src: string) => {
-	const [muted, setMuted] = useState<boolean>(false);
-	const audioRef = useRef<HTMLAudioElement>(new Audio(src));
-	audioRef.current.volume = 0.12;
-	audioRef.current.loop = true;
-	audioRef.current.muted = muted;
+	const audio = useAudio();
+	const [muted, setMuted] = useState<boolean>(audio.muted);
+
+	const changeAudio = async (src: string) => {
+		const timeStep = 50;
+		const volumeStep = 0.05;
+		const volume = audio.volume;
+		await new Promise((resolve) => {
+			const interval = setInterval(() => {
+				if (audio.volume <= 0) {
+					clearInterval(interval);
+					return resolve(null);
+				}
+				const nextVolume = audio.volume - volumeStep;
+				audio.volume = nextVolume < 0 ? 0 : nextVolume;
+			}, timeStep);
+		});
+		audio.pause();
+		audio.src = src;
+		audio.play().catch(console.error);
+		await new Promise((resolve) => {
+			const interval = setInterval(() => {
+				if (audio.volume >= volume) {
+					clearInterval(interval);
+					return resolve(null);
+				}
+				const nextVolume = audio.volume + volumeStep;
+				audio.volume = nextVolume > volume ? volume : nextVolume;
+			}, timeStep);
+		});
+	};
 
 	useEffect(() => {
-		if (audioRef.current) audioRef.current.play().catch(console.error);
-		return () => {
-			if (audioRef.current) audioRef.current.pause();
-		};
+		if (audio.paused) audio.play().catch(console.error);
 	}, []);
+
+	useEffect(() => {
+		const url = audio.src ? new URL(audio.src) : null;
+		if (url?.pathname !== src) {
+			changeAudio(src).finally();
+		}
+	}, [src]);
 
 	const toggleMuteBgSound = () => {
 		setMuted(!muted);
-		audioRef.current.muted = !muted;
+		audio.muted = !muted;
 	};
 
 	const pauseBgSound = () => {
-		audioRef.current.pause();
+		audio.pause();
 	};
 
 	const returnedValue: any = [
